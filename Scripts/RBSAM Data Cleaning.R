a9fd0131133a5217d0f0618977fcc734c72299d5
Hans Lehndorff <lehndorff@evergreenecon.com>
2018-07-12 09:00:08 -0700

    simple aggregation

--- Scripts/RBSAM Data Cleaning.R
+++ Scripts/RBSAM Data Cleaning.R
@@ -17,3 +17,23 @@ for(i in 1:9){
 
 # write.csv(paste(path,"dryers_only.csv",sep="/"))
 
+# aggregation
+library(lubridate)
+s<-proc.time()
+enduse<-read.csv("/volumes/Projects/401013 - PG&E RBSA/Metering QC/RBSAM Data/RBSAM Subsets/dryers_only.csv",stringsAsFactors = FALSE)
+proc.time()-s
+
+enduse$formtime<-as.POSIXct(enduse$time,format="%d%b%y:%H:%M:%S")
+enduse$week<-week(enduse$formtime)
+enduse$date<-date(enduse$formtime)
+enduse$hour<-hour(enduse$formtime)
+enduse$year<-year(enduse$formtime)
+
+Houragg<-enduse %>% filter(!is.na(formtime)) %>% group_by(siteid,year,week,date,hour) %>% summarise(obs=n(),kWh=sum(Dryer.kWh..Appliance.))
+Dayagg<-Houragg %>% group_by(siteid,year,week,date) %>% summarise(obs=sum(obs),kWh=sum(kWh))
+Weekagg<-Dayagg %>% group_by(siteid,year,week) %>% summarise(obs=sum(obs),kWh=sum(kWh))
+Yearagg<-Weekagg %>% group_by(siteid,year) %>% summarise(obs=sum(obs),kWh=sum(kWh))
+
+summary(Houragg$kWh)
+summary(Dayagg$kWh)
+summary(Yearagg$kWh)
