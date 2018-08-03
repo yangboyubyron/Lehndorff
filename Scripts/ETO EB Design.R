78174237702fdcfab4764293dea1e8e02cf73e5e
Hans Lehndorff <lehndorff@evergreenecon.com>
2018-08-02 17:08:23 -0700

    Add NAICS for parts, confirm SEM, drop recent survey, retain incentive, clean output.

--- Scripts/ETO EB Design.R
+++ Scripts/ETO EB Design.R
@@ -9,8 +9,41 @@ Mode <- function(x) {
 }
 
 population<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/All Eligible Commercial Sites.csv",stringsAsFactors = FALSE)
-projects<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/All Commercial Site Projects.csv",stringsAsFactors = FALSE)
+projects2<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/All Commercial Site Projects.csv",stringsAsFactors = FALSE)
 contacts<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/All Commercial Site Contacts.csv",stringsAsFactors = FALSE)
+impact_surv<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/EB 17 Sample Data Request v2_impact evaluation.csv",stringsAsFactors = FALSE)
+SEM<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/SEM impact data/SEM and Capital Participation Data to Evergreen.csv",stringsAsFactors = FALSE)
+sector_groups<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/EB market sector categories.csv",stringsAsFactors = FALSE) %>% select(et_marketname,Evergreen.categories)
+
+# assign NAICS group to population
+population$naicsgroup<-"Unknown"
+population$naicsgroup[substr(population$naics_code,1,2)==11]<-"Industrial"
+population$naicsgroup[substr(population$naics_code,1,2)==21]<-"Industrial"
+population$naicsgroup[substr(population$naics_code,1,2)==22]<-"Industrial"
+population$naicsgroup[substr(population$naics_code,1,2)==23]<-"Industrial"
+population$naicsgroup[substr(population$naics_code,1,2)==42]<-"Retail"
+population$naicsgroup[substr(population$naics_code,1,2)==44]<-"Retail"
+population$naicsgroup[substr(population$naics_code,1,3)==445]<-"Grocery"
+population$naicsgroup[substr(population$naics_code,1,2)==45]<-"Retail"
+population$naicsgroup[substr(population$naics_code,1,2)==48]<-"Warehouse"
+population$naicsgroup[substr(population$naics_code,1,2)==49]<-"Warehouse"
+population$naicsgroup[substr(population$naics_code,1,1)==5]<-"Office"
+population$naicsgroup[substr(population$naics_code,1,2)==61]<-"School"
+population$naicsgroup[substr(population$naics_code,1,2)==71]<-"Recreation"
+population$naicsgroup[substr(population$naics_code,1,1)==3]<-"Industrial"
+population$naicsgroup[substr(population$naics_code,1,2)==62]<-"Medical"
+population$naicsgroup[substr(population$naics_code,1,3)==721]<-"Hotel"
+population$naicsgroup[substr(population$naics_code,1,3)==722]<-"Restaurant"
+population$naicsgroup[substr(population$naics_code,1,3)==811]<-"Repair"
+population$naicsgroup[substr(population$naics_code,1,3)==812]<-"Repair"
+population$naicsgroup[substr(population$naics_code,1,3)==813]<-"Religious"
+population$naicsgroup[substr(population$naics_code,1,2)==92]<-"Public"
+table(population$naicsgroup,exclude = NULL)
+
+projects<-projects2 %>% left_join(sector_groups,by="et_marketname") %>% left_join(select(population,et_siteid,naicsgroup),by="et_siteid")
+table(projects$Evergreen.categories,exclude=NULL)
+
+projects$Evergreen.categories[projects$Evergreen.categories=="Unknown"&!is.na(projects$Evergreen.categories)]<-projects$naicsgroup[projects$Evergreen.categories=="Unknown"&!is.na(projects$Evergreen.categories)]
 
 table(projects$projecttrackdescription)
 table(projects$projecttrackdescription[projects$programdescription=="Existing Buildings"])
@@ -26,13 +59,21 @@ projects$date<-as.Date(projects$installeddate)
 projects$date[is.na(projects$date)]<-as.Date(projects$maxrecognizeddate[is.na(projects$date)])
 projects$year<-year(projects$date)
 
-Parts<-projects %>% filter(programdescription!="") %>% group_by(et_siteid) %>% summarise(n_measures=n(),mult_date=n_distinct(year),mult_track=n_distinct(trackval),past_year=max(year[year<2016&trackval<1000&!is.na(year)],na.rm = TRUE), recent_group=min(trackval[as.Date(date)>="2016-01-01"]),past_group=min(trackval[year==past_year],na.rm = TRUE))
+projects$confrim_SEM<-projects$projectid%in%subset(SEM,Is.SEM.customer.=="Yes")$ProjectId&projects$trackval==1
+table(projects$trackval,projects$confrim_SEM)
+
+projects$trackval[projects$trackval==1&!projects$confrim_SEM]<-1000
+
+projects$impact_survey<-projects$projectid%in%impact_surv$projectid
+
+Parts<-projects %>% filter(programdescription!="") %>% group_by(et_siteid) %>% summarise(n_measures=n(),mult_date=n_distinct(year),mult_track=n_distinct(trackval),past_year=max(year[year<2016&trackval<1000&!is.na(year)],na.rm = TRUE), recent_group=min(trackval[as.Date(date)>="2016-01-01"]),past_group=min(trackval[year==past_year],na.rm = TRUE),impact=max(impact_survey),sector=unique(Evergreen.categories),proj_incent=sum(measureincentive,na.rm = TRUE))
 Parts$segment<-paste(Parts$recent_group,"R",sep = "-")
 Parts$segment[is.infinite(Parts$recent_group)|is.na(Parts$recent_group)|Parts$recent_group==1000]<-paste(Parts$past_group[is.infinite(Parts$recent_group)|is.na(Parts$recent_group)|Parts$recent_group==1000],"P",sep="-")
 
 Parts<-Parts %>% filter(segment!="Inf-P") %>% data.frame()
 table(Parts$segment)
-
+table(Parts$segment,Parts$impact)
+table(Parts$sector)
 
 # contacts
 contacts$cont_val<-1
@@ -54,7 +95,7 @@ contacts$part_email<-contacts$CRMContactEmail
 contacts$part_email[contacts$part_email==""]<-contacts$CRMCompanyEmail[contacts$part_email==""]
 table(contacts$part_email=="")
 
-PartCon<-contacts %>% filter(et_siteid%in%Parts$et_siteid) %>% select(1:30,39:44) %>% unique() %>% filter(DoNotContact==0&CompanyDoNotContact==0&CRMContactName!=""&part_email!=""&part_phone!="") %>% group_by(et_siteid) %>% arrange(cont_val,desc(date)) %>% mutate(rank=1:n()) %>% filter(rank<=3)
+PartCon<-contacts %>% filter(et_siteid%in%Parts$et_siteid) %>% select(1:30,38:44) %>% unique() %>% filter(RecentSurvey==0&DoNotContact==0&CompanyDoNotContact==0&CRMContactName!=""&part_email!=""&part_phone!="") %>% group_by(et_siteid) %>% arrange(cont_val,desc(date)) %>% mutate(rank=1:n()) %>% filter(rank<=3)
 
 PartConagg<-PartCon %>% group_by(et_siteid) %>% summarise(n=n(),companies=n_distinct(CRMCompanyName),Primary_Contact=CRMContactName[rank==1],C1_Company=CRMCompanyName[rank==1],C1_phone=part_phone[rank==1],C1_email=part_email[rank==1],C1_Current_as_of=date[rank==1],
   C2_Name=ifelse(n>1,CRMContactName[rank==2],"NA"),C2_Company=ifelse(n>1,CRMCompanyName[rank==2],"NA"),C2_phone=ifelse(n>1,part_phone[rank==2],"NA"),C2_email=ifelse(n>1,part_email[rank==2],"NA"),
@@ -63,8 +104,10 @@ PartConagg<-PartCon %>% group_by(et_siteid) %>% summarise(n=n(),companies=n_dist
 PartFrame<-left_join(Parts,PartConagg,by="et_siteid") %>% filter(!is.na(Primary_Contact))
 
 # dedupe
-PartFrame_dedupe<-PartFrame %>% group_by(Primary_Contact) %>% arrange(desc(substr(segment,3,3)),as.numeric(substr(segment,1,1))) %>% mutate(contact_n_siteids=n(),row=1:n()) %>% filter(row==1) %>% data.frame()
+PartFrame_dedupe<-PartFrame %>% filter(sector!="Residential -- we'll probably want to exclude this") %>% group_by(Primary_Contact) %>% arrange(desc(substr(segment,3,3)),as.numeric(substr(segment,1,1))) %>% mutate(contact_n_siteids=n(),row=1:n(),any_impact=max(impact),most_common_sector=Mode(sector),n_sectors=n_distinct(sector),total_incent=sum(proj_incent)) %>% filter(row==1) %>% data.frame()
 table(PartFrame_dedupe$segment)
+table(PartFrame_dedupe$segment,PartFrame_dedupe$any_impact)
+table(PartFrame_dedupe$sector)
 
 PartFrame_dedupe$C2_Name[PartFrame_dedupe$C2_Name%in%PartFrame_dedupe$Primary_Contact]<-"NA"
 PartFrame_dedupe$C2_Company[PartFrame_dedupe$C2_Name%in%PartFrame_dedupe$Primary_Contact]<-"NA"
@@ -92,7 +135,7 @@ PartFrame_dedupe$segment[PartFrame_dedupe$segment=="5-R"]<-"Recent Lighting"
 table(PartFrame_dedupe$segment)
 
 # summary table
-PartFrame_summary<-PartFrame_dedupe %>% group_by(track=sub("[[:alpha:]]+[[:space:]]","",segment),timeframe=sub("[[:space:]][[:alpha:]]+","",segment),segment) %>% summarise(count_of_contacts=n()) %>% arrange(track,desc(timeframe)) %>% data.frame()
+PartFrame_summary<-PartFrame_dedupe %>% group_by(track=sub("[[:alpha:]]+[[:space:]]","",segment),timeframe=sub("[[:space:]][[:alpha:]]+","",segment),segment) %>% summarise(count_of_contacts=n(),impact=sum(any_impact)) %>% arrange(track,desc(timeframe)) %>% data.frame()
 # write.xlsx(PartFrame_summary,"/Users/Lehndorff/desktop/ETO_EB_Interview_Summary.xlsx",append = FALSE,sheetName = "Participants",row.names = FALSE)
 
 # write out frame by segment
@@ -101,13 +144,13 @@ j<-0
 for (i in unique(PartFrame_dedupe$segment)){
   if(WRITE!=TRUE){break}
   j<-j+1
-  frameout<-PartFrame_dedupe %>% filter(segment==i) %>% data.frame()
+  frameout<-PartFrame_dedupe %>% filter(segment==i) %>% select("et_siteid","segment","any_impact","n_sectors","most_common_sector","n_measures","total_incent","Primary_Contact","C1_Company","C1_phone","C1_email","C1_Current_as_of","C2_Name","C2_Company","C2_phone","C2_email","C3_Name","C3_Company","C3_phone","C3_email") %>% data.frame()
   if(j==1){
     write.xlsx(frameout,"/Users/Lehndorff/desktop/Part_Frame.xlsx",append = FALSE,sheetName = i,row.names = FALSE)
   }else{
     write.xlsx(frameout,"/Users/Lehndorff/desktop/Part_Frame.xlsx",append = TRUE,sheetName = i,row.names = FALSE)
   }
-  if(j=n_distinct(PartFrame_dedupe$segment)){
+  if(j==n_distinct(PartFrame_dedupe$segment)){
     j<-0
     rm(i,WRITE)
   }
