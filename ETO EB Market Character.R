c21754294a8f3d7cddcd7c70cb9dd58ff2210d88
Hans Lehndorff <lehndorff@evergreenecon.com>
2018-08-03 13:52:49 -0700

    Use market type first, add grocery

--- ETO EB Market Character.R
+++ ETO EB Market Character.R
@@ -9,66 +9,47 @@ Mode <- function(x) {
   ux[which.max(tabulate(match(x, ux)))]
 }
 
-population<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/All Eligible Commercial Sites.csv",stringsAsFactors = FALSE)
+population2<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/All Eligible Commercial Sites.csv",stringsAsFactors = FALSE)
 projects<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/All Commercial Site Projects.csv",stringsAsFactors = FALSE)
 contacts<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/All Commercial Site Contacts.csv",stringsAsFactors = FALSE)
 counties<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/Oregon and SW Washington zip codes.csv",stringsAsFactors = FALSE)
 regions<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/ETO Regions.csv",stringsAsFactors = FALSE)
 industries<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/Employment by Industry.csv",stringsAsFactors = FALSE)
+sector_groups<-read.csv("/volumes/Projects/430011 - ETO Existing Buildings/Data/EB market sector categories.csv",stringsAsFactors = FALSE) %>% select(et_marketname,Evergreen.categories)
+
+population<-population2 %>% left_join(sector_groups,by=c("market"="et_marketname"))
 
 # assign NAICS group
-population$naicsgroup<-"Unknown"
-population$naicsgroup[substr(population$naics_code,1,2)==11]<-"Industrial"
-population$naicsgroup[substr(population$naics_code,1,2)==21]<-"Industrial"
-population$naicsgroup[substr(population$naics_code,1,2)==22]<-"Industrial"
-population$naicsgroup[substr(population$naics_code,1,2)==23]<-"Industrial"
-population$naicsgroup[substr(population$naics_code,1,2)==42]<-"Retail"
-population$naicsgroup[substr(population$naics_code,1,2)==44]<-"Retail"
-population$naicsgroup[substr(population$naics_code,1,3)==445]<-"Grocery"
-population$naicsgroup[substr(population$naics_code,1,2)==45]<-"Retail"
-population$naicsgroup[substr(population$naics_code,1,2)==48]<-"Warehouse"
-population$naicsgroup[substr(population$naics_code,1,2)==49]<-"Warehouse"
-population$naicsgroup[substr(population$naics_code,1,1)==5]<-"Office"
-population$naicsgroup[substr(population$naics_code,1,2)==61]<-"School"
-population$naicsgroup[substr(population$naics_code,1,2)==71]<-"Recreation"
-population$naicsgroup[substr(population$naics_code,1,1)==3]<-"Industrial"
-population$naicsgroup[substr(population$naics_code,1,2)==62]<-"Medical"
-population$naicsgroup[substr(population$naics_code,1,3)==721]<-"Hotel"
-population$naicsgroup[substr(population$naics_code,1,3)==722]<-"Restaurant"
-population$naicsgroup[substr(population$naics_code,1,3)==811]<-"Repair"
-population$naicsgroup[substr(population$naics_code,1,3)==812]<-"Repair"
-population$naicsgroup[substr(population$naics_code,1,3)==813]<-"Religious"
-population$naicsgroup[substr(population$naics_code,1,2)==92]<-"Public"
+population$naics_code_group<-"Unknown"
+population$naics_code_group[substr(population$naics_code,1,2)==11]<-"Industrial"
+population$naics_code_group[substr(population$naics_code,1,2)==21]<-"Industrial"
+population$naics_code_group[substr(population$naics_code,1,2)==22]<-"Industrial"
+population$naics_code_group[substr(population$naics_code,1,2)==23]<-"Industrial"
+population$naics_code_group[substr(population$naics_code,1,2)==42]<-"Retail"
+population$naics_code_group[substr(population$naics_code,1,2)==44]<-"Retail"
+population$naics_code_group[substr(population$naics_code,1,3)==445]<-"Grocery"
+population$naics_code_group[substr(population$naics_code,1,2)==45]<-"Retail"
+population$naics_code_group[substr(population$naics_code,1,2)==48]<-"Warehouse"
+population$naics_code_group[substr(population$naics_code,1,2)==49]<-"Warehouse"
+population$naics_code_group[substr(population$naics_code,1,1)==5]<-"Office"
+population$naics_code_group[substr(population$naics_code,1,2)==61]<-"School"
+population$naics_code_group[substr(population$naics_code,1,2)==71]<-"Recreation"
+population$naics_code_group[substr(population$naics_code,1,1)==3]<-"Industrial"
+population$naics_code_group[substr(population$naics_code,1,2)==62]<-"Medical"
+population$naics_code_group[substr(population$naics_code,1,3)==721]<-"Hotel"
+population$naics_code_group[substr(population$naics_code,1,3)==722]<-"Restaurant"
+population$naics_code_group[substr(population$naics_code,1,3)==811]<-"Repair"
+population$naics_code_group[substr(population$naics_code,1,3)==812]<-"Repair"
+population$naics_code_group[substr(population$naics_code,1,3)==813]<-"Religious"
+population$naics_code_group[substr(population$naics_code,1,2)==92]<-"Public"
+table(population$naics_code_group)
+
+population$naicsgroup<-population$Evergreen.categories
+population$naicsgroup[population$naicsgroup=="Residential -- we'll probably want to exclude this"|is.na(population$naicsgroup)]<-"Unknown"
 table(population$naicsgroup)
 
-# then market
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Education"]<-"School"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Health"]<-"Medical"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Office"]<-"Office"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Retail"]<-"Retail"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Warehousing and Storage"]<-"Warehouse"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Unspecified Government/Public Sector"]<-"Public"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Amusement/Recreational"]<-"Recreation"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$marketsubtype=="Auto Repair"]<-"Repair"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Auto Services"]<-"Retail"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$marketsubtype=="Lodging/Hotel/Motel"]<-"Hotel"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Hospitality"]<-"Restaurant"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$marketsubtype=="Religious/Spiritual"]<-"Religious"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Assembly"]<-"Public"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Courthouse/Probation Office"]<-"Public"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Fire Protection"]<-"Public"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Library"]<-"Public"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Jail/Reformatory/Penitentiary"]<-"Public"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Military (Armory, etc.)"]<-"Public"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Police"]<-"Public"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Transportation Infrastructure (Tunnel, Roadway, Dock, etc.)"]<-"Public"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Data Center"]<-"Warehouse"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Bank/Financial Institution"]<-"Retail"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Museum"]<-"Public"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Gym/Athletic Club"]<-"Retail"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Laundry/Dry Cleaner"]<-"Retail"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Funeral/Cremation"]<-"Office"
-population$naicsgroup[population$naicsgroup=="Unknown"&population$markettype=="Parking Structure/Garage"]<-"Retail"
+population$naicsgroup[population$naicsgroup=="Unknown"]<-population$naics_code_group[population$naicsgroup=="Unknown"]
+table(population$naicsgroup)
 
 # then general
 population$naicsgroup[population$naicsgroup=="Unknown"&(population$multifamily==1|population$residential==1)]<-"Multifamily/Residential"
@@ -136,12 +117,8 @@ ggplot(population %>% filter(naicsgroup!="Unknown Commercial"))+
 
 ggplot(characterization %>% filter(participation=="Participant"))+
   theme(axis.text.x = element_text(angle = 90))+
-  geom_point(aes(x=paste(naicsgroup,fuel_size,sep=" "),y=pct,color=naicsgroup))
-
-ggplot(characterization %>% filter(participation=="Participant"))+
-  theme(axis.text.x = element_text(angle = 90))+
   geom_bar(stat = "identity",aes(x=paste(naicsgroup,fuel_size,sep=" "),y=pct,fill=naicsgroup))+
-  scale_fill_manual(values=rep(c("#ffb13d","#3e9933"),times=7))+
+  scale_fill_manual(values=rep(c("#ffb13d","#3e9933"),times=8))+
   theme(legend.position = "none")
 
 ggplot(characterization %>% filter(participation=="Participant"))+
