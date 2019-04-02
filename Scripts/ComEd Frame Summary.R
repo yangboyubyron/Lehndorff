f17d667d726ebbd93c41e7a57ee70608206129f9
Hans Lehndorff <lehndorff@evergreenecon.com>
2019-04-02 13:14:24 -0700

    Draft frame definitions

--- Scripts/ComEd Frame Summary.R
+++ Scripts/ComEd Frame Summary.R
@@ -1,12 +1,29 @@
 # ComEd Sample Frame Summary
+
+library(dplyr)
+
 load("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/usage LI and rate code.RData")
 
 frame<-w_rate %>% 
-  filter(avg*12>11000|(avg*12>9500&SFMF=="MF"))
+  filter((avg*12>11000|(avg*12>9500&SFMF=="MF"))&avg*12<=34000)
 
 frame$e_heat[frame$e_heat=="Gas Heat"]<-"Non-Elec Heat"
 frame$avg.annual<-frame$avg*12
 
+frame$usage_level<-"NOT DEFINED"
+frame$usage_level[frame$avg.annual>=9500&frame$avg.annual<11000]<-"UT11"
+frame$usage_level[frame$avg.annual>=11000&frame$avg.annual<12500]<-"UT12.5"
+frame$usage_level[frame$avg.annual>=12500&frame$avg.annual<14000]<-"UT14"
+frame$usage_level[frame$avg.annual>=14000&frame$avg.annual<=34000]<-"UT34"
+table(frame$usage_level)
+
+frame$LI_level<-ifelse(frame$LI_score>.8,"IE","non-IE")
+table(frame$LI_level)
+
+frame$Sample.Group<-paste(frame$LI_level,frame$e_heat,frame$SFMF,frame$usage_level)
+table(frame$Sample.Group)
+
+# counts should match previous sample frame summary
 frame.summary<-frame %>% 
   group_by(IE=LI_score>.8,SFMF,Heating=e_heat) %>% 
   summarise(
@@ -17,3 +34,29 @@ frame.summary<-frame %>%
   arrange(IE,desc(SFMF),Heating)
 
 # write.csv(frame.summary,"~/desktop/ComEd Frame Summary TEMP.csv")
+
+# pull sample
+set.seed(97244)
+frame.pull<-frame %>% ungroup() %>% 
+  select(ID,Sample.Group) %>% 
+  group_by(Sample.Group) %>% 
+  mutate(rand.rank=rank(runif(n())))
+
+#
+
+customers<-read.delim("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/ComEd Residential Base 101618.txt",stringsAsFactors = FALSE)
+
+# Confirm that customer ID is accurate and meaningful
+# cust.check<-customers %>% 
+#   select(ID,LOWINCOME,CENSUS_TRACT_CODE,Tariff.Rate.Typ,PRIZM.Code) %>% 
+#   filter(ID%in%w_rate$ID) %>% arrange(ID)
+#   
+# data.check<-w_rate %>% ungroup() %>% select(ID,LOWINCOME,CENSUS_TRACT_CODE,Tariff.Rate.Typ,PRIZM.Code) %>% arrange(ID)
+# 
+# identical(cust.check$ID,data.check$ID)
+# identical(cust.check$LOWINCOME,data.check$LOWINCOME)
+# identical(cust.check$PRIZM.Code,data.check$PRIZM.Code)
+# identical(cust.check$Tariff.Rate.Typ,data.check$Tariff.Rate.Typ)
+# 
+# rm(cust.check,data.check)
+
