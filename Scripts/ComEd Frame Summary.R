fdbd8c77ec2a6db79a17fc4fd959e5f7bc03ab80
Hans Lehndorff <lehndorff@evergreenecon.com>
2019-04-03 16:41:57 -0700

    Updates from completed sample memo.

--- Scripts/ComEd Frame Summary.R
+++ Scripts/ComEd Frame Summary.R
@@ -4,24 +4,46 @@ library(dplyr)
 
 load("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/usage LI and rate code.RData")
 
+# select customers with usage in samplable range
 frame<-w_rate %>% 
-  filter((avg*12>11000|(avg*12>9500&SFMF=="MF"))&avg*12<=34000)
+  filter((avg*12>11000)&avg*12<=34000)
 
+# redefine heating technology
 frame$e_heat[frame$e_heat=="Gas Heat"]<-"Non-Elec Heat"
+
+#average annual kWh
 frame$avg.annual<-frame$avg*12
 
+# usage strata
 frame$usage_level<-"NOT DEFINED"
-frame$usage_level[frame$avg.annual>=9500&frame$avg.annual<11000]<-"UT11"
-frame$usage_level[frame$avg.annual>=11000&frame$avg.annual<12500]<-"UT12.5"
-frame$usage_level[frame$avg.annual>=12500&frame$avg.annual<14000]<-"UT14"
-frame$usage_level[frame$avg.annual>=14000&frame$avg.annual<=34000]<-"UT34"
+frame$usage_level[frame$avg.annual>=11000&frame$avg.annual<14000]<-"Stratum 1"
+frame$usage_level[frame$avg.annual>=14000&frame$avg.annual<=34000]<-"Stratum 2"
 table(frame$usage_level)
 
+# IE definition n should equal 38,910
 frame$LI_level<-ifelse(frame$LI_score>.8,"IE","non-IE")
 table(frame$LI_level)
 
 frame$Sample.Group<-paste(frame$LI_level,frame$e_heat,frame$SFMF,frame$usage_level)
 table(frame$Sample.Group)
+
+# select IE high users
+frame.IE<-frame %>% filter(LI_level=="non-IE")
+table(frame.IE$Sample.Group)
+
+# add quotas
+quotas<-read.csv("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/ComEd Sample Quotas.csv",stringsAsFactors = FALSE)
+
+frame.quota<-frame.IE %>% left_join(quotas,by=c("Sample.Group"="Segment"))
+
+# assign random rank
+set.seed(97244)
+frame.rand<-frame.quota %>% ungroup() %>% 
+  group_by(Sample.Group) %>% 
+  mutate(rand.rank=rank(runif(n())))
+
+# pull sample
+frame.pull<-frame.rand %>% filter(rand.rank<=15*Quota)
 
 # counts should match previous sample frame summary
 frame.summary<-frame %>% 
@@ -34,15 +56,6 @@ frame.summary<-frame %>%
   arrange(IE,desc(SFMF),Heating)
 
 # write.csv(frame.summary,"~/desktop/ComEd Frame Summary TEMP.csv")
-
-# pull sample
-set.seed(97244)
-frame.pull<-frame %>% ungroup() %>% 
-  select(ID,Sample.Group) %>% 
-  group_by(Sample.Group) %>% 
-  mutate(rand.rank=rank(runif(n())))
-
-#
 
 customers<-read.delim("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/ComEd Residential Base 101618.txt",stringsAsFactors = FALSE)
 
