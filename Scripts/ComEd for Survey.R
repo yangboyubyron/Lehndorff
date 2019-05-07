a75f1a7b39b110ce7705bff732ae6ce1902bc051
Hans Lehndorff <lehndorff@evergreenecon.com>
2019-05-07 15:42:55 -0700

    Reflect updates to input files, save outputs.

--- Scripts/ComEd for Survey.R
+++ Scripts/ComEd for Survey.R
@@ -3,14 +3,15 @@ library(dplyr)
 library(xlsx)
 
 Contact<-read.delim("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 2/ComEd sample files with PPI/Evergreen Update 043019.txt",stringsAsFactors = FALSE)
-DNC<-read.csv("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 2/Master_DNC_List_for_ComEd_07JUN18 120618.csv",stringsAsFactors=FALSE) #Not sure what this file is.
-frame.pull<-read.csv("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/Sample Pull 0404.csv",stringsAsFactors = FALSE)
+DNC<-read.csv("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 2/Master_DNC_List_for_ComEd_07JUN18 120618.csv",stringsAsFactors=FALSE)
+frame.pull<-read.csv("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/Sample Pull 0507.csv",stringsAsFactors = FALSE)
 frame.rand<-read.csv("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/Frame With Random 0404.csv",stringsAsFactors = FALSE)
 load("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/Population All Fields.RData")
 quotas<-read.csv("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/ComEd Sample Quotas.csv",stringsAsFactors = FALSE)
 
 # Master file - an internal master file that keeps all the data we have for our own use. Usage information included at some level, including overall usage and the winter-shoulder and summer-shoulder ratios.
-master<-X
+## already accomplished at end of Frame Summary
+## add in response results and true IE status as it becomes available
 
 # Survey file - file for the survey implementer that only includes a project customer ID, the sampling bins the customer falls into, and the contact information.
 survey.frame<-frame.pull %>% left_join(Contact %>% 
@@ -26,7 +27,13 @@ mean(survey.frame$Status!="Active")
 
 survey.out<-survey.frame %>% 
   filter(Can.Contact=="Yes"&TCPA.Date!=""&Status=="Active"&DNC.ACC==FALSE&DNC.ACC==FALSE) %>% 
-  select(-ws_ratio,-avg.annual,-ACCOUNT,-Status,-Can.Contact,-TCPA.Date)
+  select(-CENSUS_TRACT_CODE,-ws_ratio,-avg.annual,-ACCOUNT,-Status,-Can.Contact,-TCPA.Date,-DNC.ACC,-DNC.Phone,
+    -M01,-M02,-M03,-M04,-M05,-M06,-M07,-M08,-M09,-M10,-M11,-M12)
+mean(survey.out$eMail!="")
+
+survey.pii<-survey.frame %>% 
+  filter(Can.Contact=="Yes"&TCPA.Date!=""&Status=="Active"&DNC.ACC==FALSE&DNC.ACC==FALSE) %>% 
+  select(-Status,-Can.Contact,-DNC.ACC,-DNC.Phone)
 
 ### survey summary
 survey.out.sum<-survey.out %>% 
@@ -34,7 +41,5 @@ survey.out.sum<-survey.out %>%
   summarise(survey.n=n()) %>% left_join(quotas,by=c("Sample.Group"="Segment")) %>% 
   mutate(ratio=survey.n/Quota) %>% arrange(ratio)
 
-# write.csv(survey.out,"/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 2/ComEd sample files with PPI/Survey Frame 0506.csv",row.names=FALSE)
-
-check.dnc<-survey.out %>% filter(ACCOUNT%in%DNC$Account)
-surv.dnc<-DNC %>% filter(Account%in%check.dnc$ACCOUNT)
+# write.csv(survey.out,"/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 2/ComEd sample files with PPI/Survey Frame External 0507.csv",row.names=FALSE)
+# write.csv(survey.pii,"/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 2/ComEd sample files with PPI/Survey Frame INTERNAL 0507.csv",row.names=FALSE)
