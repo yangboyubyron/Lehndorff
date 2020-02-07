0fe7028cac215edaaa22080e19363fef41edd860
Hans Lehndorff <lehndorff@evergreenecon.com>
2020-02-07 08:52:45 -0800

    Data for Ted

---
+++ Scripts/ComEd Reporting.R
@@ -0,0 +1,16 @@
+# ComEd Reporting
+load("/volumes/Projects/466002 - ComEd Needs Assessment/Confidential Data/Task 1/ComEd customer data anon/usage LI and rate code.RData")
+
+ted.data<-read.csv("~/desktop/ComEd/Table for Hans.csv",stringsAsFactors = FALSE)
+
+ted.fields<-w_rate %>% ungroup() %>% 
+  select(ID,ws_ratio,LOWINCOME,
+    vlow_inc_p,#
+    high_pov_p,#
+    med_inc_p,#
+    Median.Income..Household.Based._p,#
+    Net.Worth_p#
+    )
+
+for.ted<-left_join(ted.data,ted.fields,by="ID")
+write.csv(for.ted,"~/desktop/ComEd/Table for Ted.csv",row.names = FALSE)
